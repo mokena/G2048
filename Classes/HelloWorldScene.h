5f1dbd5d380840ca2139d6b9fe6261767f217217
wang rui <wangrui2401@163.com>
2016-06-06 21:18:01 +0800

    add movement listner

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,16 +2,26 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+USING_NS_CC;
 
 class HelloWorld : public cocos2d::Layer
 {
+private:
+	static const int DIRECT_UP = 1;
+	static const int DIRECT_DOWN = 2;
+	static const int DIRECT_LEFT = 3;
+	static const int DIRECT_RIGHT = 4;
+
+	Vec2 touchStart;
+	Vec2 touchEnd;
 public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    
-    // a selector callback
-    void menuCloseCallback(cocos2d::Ref* pSender);
+	bool onTouchBegan(Touch* t, Event* e);
+	void onTouchEnded(Touch* t, Event* e);
+
+	void doMove(int direction);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
